1a402760dbd263fd05d910ee8f95c7bd2c65593a
lachlanstone <lachlan.stone2@gmail.com>
2025-07-22 14:06:04 +1000

    Updated Files and implimented the PVE Directory with the status.py to check the status of a vm

---
+++ unlockrs/pve/status.py
@@ -0,0 +1,32 @@
+import requests
+import urllib3
+import json
+
+debug = False
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+#TODO Looking to migrate the pve_vmapi and pve_vmstatus to a seperate module
+#TODO Setup a loop funtion for pve scanning of the component
+
+def pve_vmstatus(Endpoint, Port, Node, vmid, token):
+# Set the URL Paramater for the Status of the VM
+    url = "https://" + Endpoint + ":" + Port + "/api2/json/nodes/" + Node + "/qemu/" + vmid + "/status/current"
+# Set the Token for the Authorization Header within the system
+    params = {
+        "Authorization": f"PVEAPIToken={token}"
+    }
+    # Disable Certifificate Verifivation
+    # r = requests.get(url, verify=False)
+    r= requests.get(url, verify=False, headers=params)
+    if r.status_code != 200:
+        print("Error, with the API got Status Code:", r.status_code)
+        print(r.reason)
+        exit()
+    elif r.status_code == 200:
+        json_data = json.loads(r.text)
+        # print(json_data["data"]["status"])
+        status = json_data["data"]["status"]
+        return status 
+    else:
+        print("Fatel Error")
+        exit()
