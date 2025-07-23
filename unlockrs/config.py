9e03af2baae2044c62acb4e168efe644cd77c6b2
lachlanstone <lachlan.stone2@gmail.com>
2025-07-23 14:14:37 +1000

    updated the components for the python program

--- unlockrs/config.py
+++ unlockrs/config.py
@@ -3,3 +3,6 @@ PVE_Port = '8006'
 PVE_Node = 'TruePVE'
 PVE_VMID = '10101'
 TrueNas_VMID = '10100'
+
+TrueNas_IP = '10.10.20.51'
+TrueNas_Pool = 'your-pool-name' # <-- TODO: Change to your ZFS pool name
