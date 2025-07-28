5b74d7dea4fd8a6adb52a57bce74884ce90b215d
lachlanstone <lachlan.stone2@gmail.com>
2025-07-28 16:28:54 +1000

    Removed old settings from Config.Py as not required anymore

--- unlockrs/config.py
+++
@@ -1,8 +0,0 @@
-PVE_Endpoint = '10.10.20.123'
-PVE_Port = '8006'
-PVE_Node = 'TruePVE'
-PVE_VMID = '10101'
-TrueNas_VMID = '10100'
-
-TrueNas_Endpoint = '10.10.20.51'
-TrueNas_Pool = 'your-pool-name' # <-- TODO: Change to your ZFS pool name
