4f005b03875388d5e5ff57b36184d99b51ff4128
lachlanstone <lachlan.stone2@gmail.com>
2025-07-28 16:28:42 +1000

    Created Yaml Import Script for the Configuration Settings for the app

---
+++ unlockrs/yaml_conf.py
@@ -0,0 +1,23 @@
+import yaml
+import os
+from unlockrs.yaml_conf import *
+
+async def load_yaml_file(dir_path="nd"):
+    dir_path = "/home/tetio/DevOps/TruePVE_UnlockRS/unlockrs/"
+    config_file = "config.yml"
+    allitems = os.listdir(dir_path)
+    if config_file in allitems:
+        file_path = os.path.join(dir_path, config_file)
+    with open(file_path) as file:
+        try:
+            data = yaml.safe_load(file)
+            print(f"Yaml Files has been loaded from {file_path}")
+            return data
+        except yaml.YAMLError as e:
+            print(f"Error loading YAML file: {e}")
+            return
+        
+async def setup_configfile(dir_path):
+    load = await load_yaml_file(dir_path=dir_path)
+# Import the TrueNas Global Variables
+    return load
