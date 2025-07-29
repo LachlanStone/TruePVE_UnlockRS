8648519a7bd357c2ef19dd174298d0f254c6eb80
lachlanstone <lachlan.stone2@gmail.com>
2025-07-29 10:30:32 +1000

    Updated System Check for multi checking so one timeout does not stop application

--- unlockrs/systemcheck.py
+++ unlockrs/systemcheck.py
@@ -5,12 +5,21 @@ async def port_check(endpoint="none", port="none"):
     assert port != "none"
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(2.0)
-    result = sock.connect_ex((endpoint, int(port)))
-    if result == 0:
-        return "online"
-    if result != 0:
-        return "offline"
-    else:
-        print("Fatel Error")
+    for i in range(5):
+        result = sock.connect_ex((endpoint, int(port)))
+        if result == 0:
+            sock.close()
+            return "online"
+        if i == 4 and result != 0:
+            sock.close()
+            print("System is Offline")
+            exit()
+        if result != 0:
+            sock.close()
+            exit()
+        else:
+            sock.close()
+            print("Fatel Error")
+            exit()
     sock.close()
     return result
